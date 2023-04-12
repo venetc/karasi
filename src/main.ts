4ab2dc6a39aa17db01ec849100faf27a6b27275f
Venetc <markedone1992@gmail.com>
2023-04-12 21:48:18 +0700

    asd

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,26 @@ import { VueQueryPlugin } from '@tanstack/vue-query';
 // @ts-ignore
 import { worker } from '../mock/browser';
 
+
+
+/*if (process.env.NODE_ENV === 'development') {
+  if (window.location.pathname === '/login') {
+    window.location.pathname = '/login/'
+    return
+  }
+  const { worker } = require('./mocks/browser')
+  await worker.start({
+    serviceWorker: {
+      url: '/login/mockServiceWorker.js',
+    },
+  })
+}*/
+
 // if (process.env.NODE_ENV === 'development') {
   worker.start({
+    serviceWorker: {
+      url: '/karasi/mockServiceWorker.js',
+    },
     onUnhandledRequest: 'bypass',
   });
 // }
